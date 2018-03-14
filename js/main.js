0aee9ce5f18da92d76b403edee401df1fca690cc
Ariana Blake <a_blake69703@fanshaweonline.ca>
2018-03-14 17:37:03 -0400

    html changed and variables created

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
+//JavaScript Document
 (() => {
-	//this is a fat arrow function
-	console.log('adding a js file');
+	console.log('Starship Troopers game fired!');
+	
+	//Variables
+	let score = 0,
+		lives = 3,
+		level = 0;
+	
+	//Functions
+	
+	
+	//Event Handling
+	
+	
+	
 })();
