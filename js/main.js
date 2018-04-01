f449d664cc258dae49a28cfb46d5d4cf246a6d4f
Hillary <h_strong69707@fanshaweonline.ca>
2018-04-01 19:24:24 -0400

    new changes to css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
+//JavaScript Document
 (() => {
-	//this is a fat arrow function
-	console.log('adding a js file');
+	console.log('Starship Troopers game fired!');
+	
+	//Variables
+	resetScreen = document.querySelector('.reset-screen')
+	
+	
+	//Functions
+	function showResetScreen(message) {
+		//user has won, reset stuff and start game over
+		resetScreen.classList.add('CHANGE THIS');
+		resetScreen.querySelector('h3').textContent = message;
+	}
+	
+	
+	//Event Handling
+	resetButton.addEventListener('click', resetGame);
+		
+	
 })();
